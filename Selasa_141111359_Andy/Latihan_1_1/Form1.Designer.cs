233987e80463160288d3a901a01afc87a3bd175b
andykoesman <andykoesman@gmail.com>
2016-10-11 22:56:57 +0700

    Latihan_1_2

--- Selasa_141111359_Andy/Latihan_1_1/Form1.Designer.cs
+++ Selasa_141111359_Andy/Latihan_1_1/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(685, 489);
+            this.tabControl1.Size = new System.Drawing.Size(416, 389);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -54,7 +54,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 23);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(677, 462);
+            this.tabPage1.Size = new System.Drawing.Size(408, 362);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -65,7 +65,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 23);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(677, 462);
+            this.tabPage2.Size = new System.Drawing.Size(408, 362);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -81,7 +81,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 513);
+            this.ClientSize = new System.Drawing.Size(440, 413);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
