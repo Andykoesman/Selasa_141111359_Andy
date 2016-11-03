c94f92af618cc7dc3ccfe4f036a36f20713ea2b9
andykoesman <andykoesman@gmail.com>
2016-11-03 23:11:20 +0700

    Latihan_5_1

--- Selasa_141111359_Andy/Latihan_5_1/Form1.cs
+++ Selasa_141111359_Andy/Latihan_5_1/Form1.cs
@@ -8,13 +8,58 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Latihan_4_1
+namespace Latihan_5_1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public Color richtextboxparent
+        {
+            get { return this.richTextBox1.BackColor; }
+            set { this.richTextBox1.BackColor = value; }
+        }
+
+            private void newToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Form1 baru = new Form1();
+            baru.Show();
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog buka = new OpenFileDialog();
+            buka.Filter = "Text|*.txt|Document|*.docx|rich Text|*.rtf";
+            buka.Title = "Open File";
+            buka.FileName = "Choose File";
+            buka.ShowDialog();
+            if (buka.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(buka.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog simpan = new SaveFileDialog();
+            simpan.Filter = "Text|*.txt|Document|*.docx|rich Text|*.rtf";
+            simpan.Title = "Save File";
+            simpan.ShowDialog();
+            if (simpan.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(simpan.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are You Sure ?", "Exit", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -132,58 +177,11 @@ namespace Latihan_4_1
             }
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        private void editorToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
-
-        private void newToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Form1 baru = new Form1();
-            baru.Show();
-        }
-
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog buka = new OpenFileDialog();
-            buka.Filter = "Text|*.txt|Document|*.docx|rich Text|*.rtf";
-            buka.Title = "Open File";
-            buka.FileName = "Choose File";
-            buka.ShowDialog();
-            if (buka.ShowDialog() == DialogResult.OK)
-            {
-                richTextBox1.LoadFile(buka.FileName, RichTextBoxStreamType.PlainText);
-            }
-        }
-
-        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog simpan = new SaveFileDialog();
-            simpan.Filter = "Text|*.txt|Document|*.docx|rich Text|*.rtf";
-            simpan.Title = "Save File";
-            simpan.ShowDialog();
-            if (simpan.ShowDialog() == DialogResult.OK)
-            {
-                richTextBox1.SaveFile(simpan.FileName, RichTextBoxStreamType.PlainText);
-            }
-        }
-
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (MessageBox.Show("Are You Sure ?", "Exit", MessageBoxButtons.OKCancel) == DialogResult.OK)
-            {
-                Application.Exit();
-            }
-        }
-
-        private void fIleToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
+            Form2 form = new Form2(this);
+            form.MdiParent = this.ParentForm;
+            form.Show();
         }
     }
 }
